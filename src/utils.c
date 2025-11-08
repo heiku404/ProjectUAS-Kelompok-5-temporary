c17cb34f8f3bf7fd42223ee4af2601aa6613d294
fahrilbelajar <fahril56@mhs.usk.ac.id>
2025-11-08 10:36:27 +0700

    structnya nih

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
-int main(){
-    printf("Hello, World!\n");
-    printf("tes bang");
-    return 0;
-}
+typedef struct {
+    unsigned int id;
+    char name[50];
+    char merek[30];
+    char model[30];
+    unsigned int tahun_produksi;
+    unsigned int jumlah_unit;
+} Alat;
