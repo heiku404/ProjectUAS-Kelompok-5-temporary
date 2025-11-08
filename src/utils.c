6cf71e4aba08888bcfb00f8e1249499216129117
fahrilbelajar <fahril56@mhs.usk.ac.id>
2025-11-08 23:53:28 +0700

    fungsi terakhir done

--- src/utils.c
+++ src/utils.c
@@ -1,18 +1,30 @@
-typedef struct {
-    unsigned int id;
-    char name[50];
-    char merek[30];
-    char model[30];
-    unsigned int tahun_produksi;
-    unsigned int jumlah_unit;
-} Alat;
-
-int filekosong (const char*utils){
-    
-    FILE *fp = fopen ("utils.txt", "r");
+#include <stdio.h>
+#include "utils.h"
+int fileKosong (const char*namaFile){
+    FILE *fp = fopen ("alat.txt", "r");
     if (fp == NULL){
         return 1;
     }
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fclose(fp);
+
+    if (size == 0)
+        return 1;  // kosong
+    else
+        return 0;  // ada isi
 }
 
- 
+int cariAlatById(unsigned int id, Alat *out) {
+    FILE *fp = fopen("alat.txt", "r");
+    if (fp == NULL){
+        printf("file tidak ditemukan");
+        return 0;
+    }
+    char line[256];
+    while(fgets(line,sizeof(line), fp)!= NULL){
+        unsigned int tid, ttahun, tjumlah;
+        char tnama[50], tmerek[30], tmodel[30];
+
+        int parsed = sscanf(line, %u|%49[^|]|%29[^|]|%29[^|]|%u|%u"
+    }
